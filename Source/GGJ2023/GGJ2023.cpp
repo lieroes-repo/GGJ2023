f69065e5bdf41271a320e824f66410565053c374
lieroes <lars-betting@protonmail.com>
2023-01-28 13:59:46 +0000

    + StarterProject UE5
    Set up project with UE5 + gitignore

---
+++ Source/GGJ2023/GGJ2023.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GGJ2023.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GGJ2023, "GGJ2023" );
